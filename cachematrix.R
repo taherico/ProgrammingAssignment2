2ea47fc412003ab8024195a9b0a8bce3fdaa3682
Saeed <saeed.taherian@gmail.com>
2018-01-14 09:56:51 -0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## calculate matrix invesion is computationally heavy
+## so, keeping the calculated inversed matrix and retrieve that 
+## when we need, could decrease computational time
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function create a vector that its members are functions: get, set, getInversedMAtrix, setInversedMatrix
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+  	inversedMatrix <- NULL
+  	set <- function(y)
+  	{
+    		x <<- y
+   		 inversedMatrix <<- NULL
+  	}
+  	get <- function() x
+  	setInversedMatrix <- function(inversed)
+ 	{
+    		inversedMatrix <<- inversed
+  	}
+  	getInversedMatrix <- function() inversedMatrix 
+  	list(set = set, get = get, setInversedMatrix = setInversedMatrix,  getInversedMatrix = getInversedMatrix)
 }
 
 
-## Write a short comment describing this function
+## This method is used to Calculate inversed matrix if inversedMatrix is NULL
+##else it would retrieve inversedMatrix without need to calculation
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  	inv <- x$getInversedMatrix ()
+  	if(!is.null(inv))
+  	{
+  	  	message("No Need To Calculate!")
+  	  	return(inv)
+ 	 }
+ 	 data <- x$get()
+ 	 inv <- solve(data)
+ 	 x$setInversedMatrix (inv)
+ 	 inv
 }
